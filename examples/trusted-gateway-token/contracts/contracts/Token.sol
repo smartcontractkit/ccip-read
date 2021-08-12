4eb2766774933591620aeced6f6a28a7b58ba7f1
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-08-12 12:11:48 +0100

    Add token example

---
+++ examples/trusted-gateway-token/contracts/contracts/Token.sol
@@ -0,0 +1,86 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title Token
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ * Based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.1/contracts/examples/SimpleToken.sol
+ */
+contract Token is ERC20, Ownable {
+  string public url;
+  address private _signer;
+  mapping(address=>bool) claimed;
+  error OffchainLookup(string url, bytes prefix);
+  struct BalanceProof {
+    bytes signature;
+    uint balance;
+  }
+
+  /**
+    * @dev Constructor that gives msg.sender all of existing tokens.
+    */
+  constructor(
+      string memory name,
+      string memory symbol,
+      uint256 initialSupply
+  ) ERC20(name, symbol) {
+      _mint(msg.sender, initialSupply);
+  }
+
+  function setUrl(string memory url_) external onlyOwner{
+    url = url_;
+  }
+
+  function setSigner(address signer_) external onlyOwner{
+    _signer = signer_;
+  }
+
+  function getSigner() external view returns(address){
+    return _signer;
+  }
+
+  function balanceOf(address addr) public override view returns(uint balance) {
+    if(claimed[addr]){
+      return super.balanceOf(addr);
+    }else{
+      revert OffchainLookup(url, abi.encodeWithSelector(Token.balanceOfWithProof.selector, addr));
+    }
+  }
+
+  function balanceOfWithProof(address addr, BalanceProof memory proof) external view returns(uint) {
+    address recovered = recoverAddress(addr, proof);
+    require(_signer == recovered, "Signer is not the domain owner");
+    return proof.balance;
+  }
+
+  function recoverAddress(address addr, BalanceProof memory proof) internal pure returns(address) {
+    (uint8 v, bytes32 r, bytes32 s) = splitSignature(proof.signature);
+    bytes32 messageHash = keccak256(abi.encodePacked(proof.balance, addr));
+    bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+    return ecrecover(ethSignedMessageHash, v, r, s);
+  }
+
+  /// signature methods.
+  function splitSignature(bytes memory sig)
+      internal
+      pure
+      returns (uint8 v, bytes32 r, bytes32 s)
+  {
+      require(sig.length == 65);
+      assembly {
+          // first 32 bytes, after the length prefix.
+          r := mload(add(sig, 32))
+          // second 32 bytes.
+          s := mload(add(sig, 64))
+          // final byte (first byte of the next 32 bytes).
+          v := byte(0, mload(add(sig, 96)))
+      }
+      return (v, r, s);
+  }
+}
---
+++ examples/trusted-gateway-token/contracts/test/token-test.js
@@ -0,0 +1,59 @@
+const { expect } = require("chai");
+
+describe("Token", function () {
+  let initial_supply, name, symbol, owner, signer, account2, addresses, Token, token, url
+  before(async () => {
+    initial_supply = 1
+    name = 'Test'
+    symbol = 'TXT'
+    url = 'https://someurl.com'
+    addresses = await ethers.getSigners();
+    [owner, signer, account2] = addresses
+    Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy(name, symbol, initial_supply);
+    await token.deployed();
+  });
+
+  it("initial attributes", async function () {
+    expect(await token.name()).to.equal(name);
+    expect(await token.owner()).to.equal(owner.address);
+  });
+
+  it("owner sets a signer", async function (){
+    const tx = await token.setSigner(signer.address);
+    await tx.wait();
+    expect(await token.getSigner()).to.equal(signer.address);
+  })
+
+  it("owner sets a url", async function (){
+    const tx = await token.setUrl(url);
+    await tx.wait();
+    expect(await token.url()).to.equal(url);
+  })
+
+  it("balanceOf throws OffchainLookup error", async function (){
+    try {
+      await token.balanceOf(owner.address);
+    } catch (error) {
+      console.log(error.message);
+      expect(error.message).to.match(/OffchainLookup/)
+    }
+  })
+
+  it("balanceOfWithProof", async function (){
+    const balance = 2
+    messageHash = ethers.utils.solidityKeccak256(
+      ['uint', 'address'],[balance3, account2.address]
+    );
+    let messageHashBinary = ethers.utils.arrayify(messageHash);
+    let signature = await signer.signMessage(messageHashBinary);
+    proof = {
+      signature,
+      balance
+    };
+
+    let newBalance = await token.balanceOfWithProof(account2.address, proof)
+    expect(newBalance).to.equal(balance);
+  })
+
+});
