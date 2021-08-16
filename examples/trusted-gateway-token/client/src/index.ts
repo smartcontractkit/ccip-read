b7f5829657b36c745758410e4ae34ac9b271f0c9
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-08-16 08:43:46 +0100

    WIP

--- examples/trusted-gateway-token/client/src/index.ts
+++ examples/trusted-gateway-token/client/src/index.ts
@@ -1,9 +1,11 @@
 const nodeFetch = require('node-fetch');
 const ethers = require('ethers');
 const jayson = require('jayson');
-require('dotenv').config()
+require('dotenv').config({ path: '../.env' })
+
 const{
   TOKEN_ADDRESS,
+  PROVIDER_URL,
   USER_ADDRESS
 } = process.env
 
@@ -14,20 +16,32 @@ const abi = [
   "function balanceOf(address addr) view returns (uint256)",
   "function balanceOfWithProof(address addr, uint256 balance, bytes proof) view returns (uint256)"
 ];
-
-const iface = new ethers.utils.Interface(abi)
-const data = iface.encodeFunctionData("balanceOf(address addr)", [USER_ADDRESS])
-const body = {
-  jsonrpc: '2.0',
-  method: 'durin_call',
-  params: [{to:TOKEN_ADDRESS, data}],
-  id: 1,
+async function main(){
+  const provider = new ethers.providers.JsonRpcProvider(PROVIDER_URL);
+  const erc20 = new ethers.Contract(TOKEN_ADDRESS, abi, provider);
+  try{
+    const balance = await erc20.balanceOf(USER_ADDRESS);
+    console.log({balance})
+  }catch(e){
+    console.log({e})
+  }
 }
 
-nodeFetch('http://localhost:8080/rpc', {
-  method: 'post',
-  body:    JSON.stringify(body),
-  headers: { 'Content-Type': 'application/json' },
-})
-.then((res:any) => res.json())
-.then((json:any) => console.log(json));
+main()
+// const iface = new ethers.utils.Interface(abi)
+// console.log({USER_ADDRESS, TOKEN_ADDRESS})
+// const data = iface.encodeFunctionData("balanceOf(address addr)", [USER_ADDRESS])
+// const body = {
+//   jsonrpc: '2.0',
+//   method: 'durin_call',
+//   params: [{to:TOKEN_ADDRESS, data}],
+//   id: 1,
+// }
+
+// nodeFetch('http://localhost:8080/rpc', {
+//   method: 'post',
+//   body:    JSON.stringify(body),
+//   headers: { 'Content-Type': 'application/json' },
+// })
+// .then((res:any) => res.json())
+// .then((json:any) => console.log(json));
--- examples/trusted-gateway-token/server/src/index.ts
+++ examples/trusted-gateway-token/server/src/index.ts
@@ -1,7 +1,9 @@
 const durin = require('@ensdomains/durin');
 const ethers = require('ethers');
 const server = new durin.Server();
-require('dotenv').config()
+const fs = require('fs');
+require('dotenv').config({ path: '../.env' })
+
 const abi = [
   "function balanceOf(address addr) view returns (uint256)",
   "function balanceOfWithProof(address addr, uint256 balance, bytes proof) view returns (uint256)"
@@ -9,19 +11,36 @@ const abi = [
 const{
   PROVIDER_URL,
   PRIVATE_KEY,
-  TOKEN_ADDRESS
+  ADDRESS_FILE_PATH
 } = process.env
+console.log({
+  PROVIDER_URL,
+  PRIVATE_KEY,
+  ADDRESS_FILE_PATH
+})
 const provider = new ethers.providers.JsonRpcProvider(PROVIDER_URL);
+console.log(1)
 let signer = new ethers.Wallet(PRIVATE_KEY);
-
+console.log(2)
+const data = fs.readFileSync(ADDRESS_FILE_PATH, 'utf8')
+  .split("\n")
+  .slice(1) // Remove the header
+  .map((d:string) => d.split(","))
+  .filter((r:string[]) => r[0] !== '')
+  console.log(3)
+const balances = data.reduce((map:any, obj:any) => {
+  const [key, val] = obj
+  map[key] = parseInt(val);
+  return map;
+}, {});
+console.log(4, {balances})
 server.add(abi, [
   {
     calltype: 'balanceOf',
     returntype: 'balanceOfWithProof',
-    func: async (contractAddress:string, addresses:string[]) => {
+    func: async (_contractAddress:string, addresses:string[]) => {
       const addr = addresses[0]
-      const erc20 = new ethers.Contract(contractAddress, abi, provider);
-      const balance = await erc20.balanceOf(addr);      
+      const balance = balances[addr] || 0
       let messageHash = ethers.utils.solidityKeccak256(
         ['address', 'uint256'],[addr, balance]
       );
@@ -30,6 +49,6 @@ server.add(abi, [
       return [addr, balance, sig];
     }
   }
-], TOKEN_ADDRESS);
+], '');
 const app = server.makeApp('/rpc');
 app.listen(8080);
